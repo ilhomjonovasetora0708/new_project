1264629043eab647fe14ba6e1119f5790c328ba0
setorailhomjonova0708 <setorailhomjonova@gmail.com>
2026-05-12 23:18:15 +0500

    tugallandi

---
+++ main.js
@@ -0,0 +1,17 @@
+function scrollToProjects() {
+    document.getElementById("projects").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+
+    if (document.body.classList.contains("light-mode")) {
+        themeBtn.innerHTML = "☀️";
+    } else {
+        themeBtn.innerHTML = "🌙";
+    }
+});
